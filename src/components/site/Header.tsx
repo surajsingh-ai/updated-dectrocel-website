c8b03ae33230c17e6db37eba3427a67de0f8f145
Suraj.xd1054 <thakursuraj5454@gmail.com>
2026-08-01 17:25:20 +0530

    Center header navigation

--- src/components/site/Header.tsx
+++ src/components/site/Header.tsx
@@ -27,7 +27,7 @@ export function Header() {
             <span className="block whitespace-nowrap">Dectrocel Healthcare</span>
             <span className="block whitespace-nowrap">and Research Pvt. Ltd</span>
           </Link>
-          <nav className="hidden min-w-0 flex-1 items-center justify-end gap-4 lg:flex xl:gap-5">
+          <nav className="hidden min-w-0 flex-1 items-center justify-center gap-4 lg:flex xl:gap-5">
             {links.map((l) => (
               <Link
                 key={l.to}
